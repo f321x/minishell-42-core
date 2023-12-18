100201fedec63e6558831c9e2d9ef1fd8815d718
fbock <fbock@student.42heilbronn.de>
2023-12-18 09:19:00 +0100

    alias commit

---
+++ includes/minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 09:07:45 by ***REMOVED***             #+#    #+#             */
+/*   Updated: 2023/12/18 09:15:20 by ***REMOVED***            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <termios.h>
