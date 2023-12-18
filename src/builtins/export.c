60dae7a8c9cb2d03bc70ae461fc9eaac5fecf475
fbock <fbock@student.42heilbronn.de>
2023-12-18 16:18:16 +0100

    alias commit

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,13 +6,20 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:43:49 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2023/12/18 15:46:49 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2023/12/18 16:18:06 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// call for each pair of variable=value to export it to the env
 bool	export(const char *env_var, const char *var_val)
 {
+	extern char **environ;
 
+	for (int i = 0; environ[i]; i++)
+	{
+		printf("%s\n", environ[i]);
+	}
+	return (true);
 }
