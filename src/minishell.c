5f7b54933fc52c9c88fbbc3e526d762b63e1e04b
fbock <fbock@student.42heilbronn.de>
2023-12-18 13:10:28 +0100

    alias commit

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 09:28:44 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2023/12/18 09:29:31 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2023/12/18 13:06:36 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 
 int	main(int argc, char **argv, char **envp)
 {
+	t_pipe pipe_struct;
+
+	// handlers
+
+	while (1)
+	{
+		pipe_struct = parse_line(void);
+		execute_line(pipe_struct);
+	}
 
 	return (0);
 }
