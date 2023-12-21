c5bebf7c7622daaedd009c5c54a4a1859e966c75
fbock <fbock@student.42heilbronn.de>
2023-12-21 13:25:51 +0100

    alias commit

--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 16:34:57 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2023/12/21 12:59:33 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2023/12/21 13:00:24 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,37 @@
 
 char **append_string(char **orig, char *str)
 {
-    size_t	amount;
-    long	index;
-    char	**new_str_arr;
+	size_t	amount;
+	long	index;
+	char	**new_str_arr;
 
-    amount = 0;
-    index = 0;
-    while (orig && orig[amount] != NULL)
-        amount++;
-    new_str_arr = malloc(((sizeof(char *) + 1) * amount) + sizeof(void *));
-    if (!new_str_arr)
-        return (NULL);
-    while (index < amount)
-    {
-        new_str_arr[index] = orig[index];
-        index++;
-    }
-    new_str_arr[index] = ft_strdup(str);
-    if (!new_str_arr[index])
-    {
-        while (index > 0)
-        {
-            free(new_str_arr[index]);
-            index--;
-        }
-        free(new_str_arr);
-        return (NULL);
-    }
-    new_str_arr[index + 1] = NULL;
-    if (amount > 0)
-    {
-        for (index = 0; index < amount; index++)
-        {
-            free(orig[index]);
-        }
-        free(orig);
-    }
-    return (new_str_arr);
+	amount = 0;
+	index = 0;
+	while (orig && orig[amount] != NULL)
+		amount++;
+	new_str_arr = malloc(((sizeof(char *) + 1) * amount) + sizeof(void *));
+	if (!new_str_arr)
+		return (NULL);
+	while (index < amount)
+	{
+		new_str_arr[index] = orig[index];
+		index++;
+	}
+	new_str_arr[index] = ft_strdup(str);
+	if (!new_str_arr[index])
+	{
+		while (index > 0)
+		{
+			free(new_str_arr[index]);
+			index--;
+		}
+		free(new_str_arr);
+		return (NULL);
+	}
+	new_str_arr[index + 1] = NULL;
+	if (amount > 0)
+		free(orig);
+	return (new_str_arr);
 }
 
 bool	ft_isdelimiter(char c)
