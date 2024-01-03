42aebef816415c91412911d57d8ebc52daca1015
fbock <fbock@student.42heilbronn.de>
2024-01-03 18:07:36 +0100

    alias commit

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 09:28:44 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2024/01/03 17:57:18 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2024/01/03 18:06:25 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,15 +40,18 @@ int	main(int argc, char **argv, char **envp)
 		if (!parsing_main(entered_line, &task))
 			continue ;
 
-		// for (size_t i = 0; i < task.p_amount; i++)
-		// {
-		// 	printf("process %zu: %s\n", i, task.processes[i].name);
-		// 	while (task.processes[i].argv && *task.processes[i].argv)
-		// 	{
-		// 		printf("argv: %s\n", *task.processes[i].argv);
-		// 		task.processes[i].argv++;
-		// 	}
-		// }
+		for (size_t i = 0; i < task.p_amount; i++)
+		{
+			printf("argv:");
+			while (task.processes[i].argv && *task.processes[i].argv)
+			{
+				printf(" %s", *task.processes[i].argv);
+				task.processes[i].argv++;
+			}
+			printf("\n");
+		}
+		continue;
+
 		execute_line(&task);
 		// free(entered_line);
 		continue ;
