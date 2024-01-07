71e57413dcda964a8543ae0a32a399cff650b15a
Mara Schulze <marschul@2-H-5.42heilbronn.de>
2024-01-07 16:55:19 +0100

    fixed small things in exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: marschul <marschul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 17:59:20 by marschul          #+#    #+#             */
-/*   Updated: 2024/01/01 18:45:40 by marschul         ###   ########.fr       */
+/*   Updated: 2024/01/07 16:48:53 by marschul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,33 @@
 
 bool	_exit_(char **argv)
 {
-	int	exit_value;
+	unsigned char	exit_value;
 	int	i;
 
 	assert(argv != NULL && ft_strcmp(argv[0], "exit") == 0); // debug
 
+	write(1, "exit\n", 5);
 	if (argv[1] != NULL)
 	{
-		exit_value = (char) ft_atoi(argv[1]);
-		i = 0;
-		while (argv[1][i] != '\0')
+		if (argv[1][0] == '\0' || (argv[1][0] != '-' && argv[1][0] != '+' && ft_isdigit(argv[1][0]) != 1))
 		{
-			if (ft_isdigit(argv[1][i]) == 0)
+			ft_printf("Minishell: numeric argument required.\n");
+			exit_value = 255;
+		}
+		else
+		{
+			exit_value = (unsigned char) ft_atoi(argv[1]);
+			i = 1;
+			while (argv[1][i] != '\0')
 			{
-				ft_printf("Minishell: Numeric argument required.\n");
-				exit_value = 255;
-				break;
+				if (ft_isdigit(argv[1][i]) == 0)
+				{
+					ft_printf("Minishell: numeric argument required.\n");
+					exit_value = 255;
+					break;
+				}
+				i++;
 			}
-			i++;
 		}
 	}
 	else
