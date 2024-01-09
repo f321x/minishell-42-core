7bc0ace63165d450c7478ac1a45891ac76ee943d
fbock <fbock@student.42heilbronn.de>
2024-01-09 16:02:05 +0100

    alias commit

--- src/input_handling/input_utils.c
+++ src/input_handling/input_utils.c
@@ -6,12 +6,17 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 13:38:47 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2024/01/09 14:20:17 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2024/01/09 16:01:54 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// Reads a line of input from the user.
+// prompt: The prompt to display to the user.
+// exits if the user presses CTRL+D (EOF / 0)
+// if entered line is empty it will be freed and the the loop waits for input
+// returns the entered line if it is not empty
 char *read_a_line(char *prompt)
 {
 	char	*entered_line;
@@ -19,6 +24,11 @@ char *read_a_line(char *prompt)
 	while (1)
 	{
 		entered_line = readline(prompt);  // 0 or null terminator? ctrl+d
+		if (entered_line == 0)
+		{
+			free(entered_line);
+			exit(EXIT_SUCCESS);
+		}
 		if (!entered_line)  // some kind of error
 			continue ;
 		if (!(*entered_line))  // empty line, will not be used
--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 09:28:44 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2024/01/09 15:27:13 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2024/01/09 15:53:36 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,6 @@ int	main(int argc, char **argv, char **envp)
 
 		// reading the input in a heap char array
 		entered_line = read_a_line(SHELL_PROMPT);
-
 		add_history(entered_line);
 		if (!entered_line)
 			continue ;
