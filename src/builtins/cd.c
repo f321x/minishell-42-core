73bf30075d97eefd55bcb2d4ed6d6bf38bfd0c14
Mara Schulze <marschul@3-B-11.42heilbronn.de>
2024-01-10 14:52:28 +0100

    Implemented PWD changing in cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
+/*   By: marschul <marschul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:16:14 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2024/01/10 13:37:06 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2024/01/10 14:51:51 by marschul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+bool	set_pwd(void)
+{
+	char	cwd[PATH_MAX];
+	char	*pwd;
+	char	*argv[3];
+
+	if (getcwd(cwd, PATH_MAX) == NULL)
+		return (false);
+	pwd = ft_strjoin("PWD=", cwd);
+	if (pwd == NULL)
+		return (false);
+	argv[0] = ft_strdup("export");
+	argv[1] = pwd;
+	argv[2] = NULL;
+	if (! export(argv))
+		return (false);
+	return (true);
+}
 
 /*
 cd works with relative paths.
@@ -33,6 +52,8 @@ bool	cd(char **argv)
 		return (error_wrapper());
 	error = chdir(entered_path_arg);
 	if (error == -1)
+		return (error_wrapper());
+	if (! set_pwd())
 		return (error_wrapper());
 	return (true);
 }
